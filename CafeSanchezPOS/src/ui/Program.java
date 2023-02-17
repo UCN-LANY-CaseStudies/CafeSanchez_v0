9acc0dbffec5cf5b0b35881e3d2e6d9340586d5d
Lars Nysom <LANY@ucn.dk>
2023-02-17 11:22:52 +0100

    cleanup

--- CafeSanchezPOS/src/ui/Program.java
+++ CafeSanchezPOS/src/ui/Program.java
@@ -6,6 +6,7 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		// Opens main window
 		MainWindow main = new MainWindow(OrderHandlingController.getInstance()); 
 		main.setVisible(true);
 	}
